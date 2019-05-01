bb0f70a2001ffe1563b1ae69a4ef92eba8f58ac4
Rafa B <rafa.betita@gmail.com>
2019-05-01 09:20:43 -0700

    add player struct

--- video_poker/poker.cpp
+++ video_poker/poker.cpp
@@ -5,6 +5,7 @@
 #include "poker.h"
 
 void poker::main() {
+    initGame();
     std::string faceValue[13], suitValue[4];
     bool deck[52];
     unsigned int hand[5];
@@ -14,15 +15,19 @@ void poker::main() {
 
     do
     {
-        dealAHand(deck, hand);
-        displayHand(hand, suitValue, faceValue);
-        hand[0] = 1;
-        hand[1] = 2;
-        hand[2] = 3;
-        hand[3] = 4;
-        hand[4] = 5;
+        displayMainWindow();
+//        dealAHand(deck, hand);
+//        displayHand(hand, suitValue, faceValue);
+//        hand[0] = 1;hand[1] = 2;hand[2] = 3;hand[3] = 4;hand[4] = 5;
+//        scoreHand(hand);
+        hand[0] = 13;hand[1] = 23;hand[2] = 25;hand[3] = 24;hand[4] = 22; // flush
+//        hand[0] = 12;hand[1] = 32;hand[2] = 34;hand[3] = 26;hand[4] = 13; // flush
         scoreHand(hand);
+
+        displayHand(hand, suitValue, faceValue);
+
     } while (again("Do you want to do this again? "));
+    exitGame();
 
 }
 
@@ -45,8 +50,6 @@ void poker::loadCardValueAndSuits(std::string *faceValue, std::string *suitValue
     faceValue[11] = "Queen";
     faceValue[12] = "King";
 }
-
-
 
 void poker::shuffleDeck(bool *deck) {
     std::cout << "Shuffling cards..." << std::endl;
@@ -96,84 +99,94 @@ void poker::dealAHand(bool *deck, unsigned int *hand) {
 void poker::displayHand(unsigned int *hand, std::string *suitValue, std::string *faceValue) {
     std::cout << "Your current hand is: \n";
     for (unsigned int i = 0; i < 5; ++i)
-        std::cout << faceValue[hand[i]%13] << " of " << suitValue[hand[i]/13] << std::endl;
-    std::cout << std::endl << std::endl;
+        std::cout << faceValue[hand[i]%13] << " of "
+                  << suitValue[hand[i]/13];
 }
 
 void poker:: scoreHand(unsigned int *hand) {
-    unsigned int faceValue[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0},
-                 suitValue[4] = {0,0,0,0},
-                 pairs = 0;
-    bool royalStraightFlush = false,
-         lastTwoAceKing = false,
-         fourOfAKind = false,
-         threeOfAKind = false,
-         straight = false,
-         flush = false,
-         fullHouse = false;
+
+    playerHand my;
+
 
     for (size_t i = 0; i < 5; ++i) {
-        ++faceValue[hand[i]%13];
-        ++suitValue[hand[i]/13];
+
+        ++my.faceValue[hand[i]%13];
+        ++my.suitValue[hand[i]/13];
     }
 
-    checkForHand(faceValue, suitValue, fourOfAKind, threeOfAKind, pairs, straight, flush, lastTwoAceKing);
+    checkForHand(&my);
 
-    fullHouse = threeOfAKind & (pairs == 1);
-    royalStraightFlush = lastTwoAceKing & straight & flush;
+    my.fullHouse = my.threeOfAKind & (my.pairs == 1);
+    my.royalStraightFlush = my.hasAceKing & my.straight & my.flush;
 
-    if (royalStraightFlush)
+    if (my.royalStraightFlush)
         std::cout << "Royal straight flush" << std::endl;
-    if (lastTwoAceKing)
+    if (my.hasAceKing)
         std::cout << "Last Two Ace King" << std::endl;
-    if (fourOfAKind)
+    if (my.fourOfAKind)
         std::cout << "Four of a Kind" << std::endl;
-    if (threeOfAKind)
+    if (my.threeOfAKind)
         std::cout << "Three of a kind" << std::endl;
-    if (straight)
+    if (my.straight)
         std::cout << "Straight" << std::endl;
-    if (flush)
+    if (my.flush)
         std::cout << "Flush" << std::endl;
-    if(fullHouse)
+    if (my.fullHouse)
         std::cout << "Player has full house" << std::endl;
-    if (pairs)
-        std::cout << "Player has " << pairs << ((pairs == 1) ? " pair" : " pairs") << std::endl;
+    if (my.pairs)
+        std::cout << "Player has " << my.pairs << ((my.pairs == 1) ? " pair" : " pairs") << std::endl;
 
 }
 
-void poker::checkForHand(unsigned int *faceValue, unsigned int *suitValue, bool &fourOfAKind, bool &threeOfAKind,
-                         unsigned int &pairs, bool &straight, bool &flush, bool &lastTwoAceKing) {
-    threeOfAKind = fourOfAKind = false;
-    pairs = 0;
-    straight = flush = false;
+void poker::checkForHand(playerHand *my) {
 
     // Checks for pairs
     for (size_t i = 0; i < 13; ++i) {
-        switch(faceValue[i]) {
-            case 2: ++pairs;
+        switch(my->faceValue[i]) {
+            case 2: ++my->pairs;
                     break;
-            case 3: threeOfAKind = true;
+            case 3: my->threeOfAKind = true;
                     break;
-            case 4: fourOfAKind = true;
+            case 4: my->fourOfAKind = true;
 
         }
     }
     
-    // Check for flush
+    // Check for flush (5 cards of the same suit)
     for (size_t j = 0; j < 4; ++j) {
-        flush |= suitValue[j] == 4;
+        my->flush |= my->suitValue[j] == 5;
     }
-
-    // Check for flush
-    for (size_t k = 0; k < 9 && !straight; ++k) {
-        straight = faceValue[k] &
-                   faceValue[k+1] &
-                   faceValue[k+2] &
-                   faceValue[k+3] &
-                   faceValue[k+4 % 13];
-        lastTwoAceKing = faceValue [k+3]%13 == 12 &
-                         faceValue [(k+3%13)] == 0;
+    //     unsigned int faceValue[13] = {1,0,0,0,0,0,0,0,0,1,1,1,1},
+    // Check for royal flush
+    for (size_t k = 0; k < 10 && !my->straight; ++k) {
+        // A straight implies that there are 5 '1's in a row, so an & operator on all of them should result in a 1.
+        // 0 if the series includes a 0.
+        my->straight = my->faceValue[k]   &
+                       my->faceValue[k+1] &
+                       my->faceValue[k+2] &
+                       my->faceValue[k+3] &
+                       my->faceValue[(k+4) % 13];
     }
+    my->hasAceKing = my->faceValue [12] == 1 &
+                     my->faceValue [0] == 1;
 }
 
+void poker::initGame() {
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);
+}
 
+void poker::exitGame() {
+    endwin();
+}
+
+void poker::displayMainWindow() {
+    int height = 22;
+    int width = 78;
+    int xpos = 1;
+    int ypos = 1;
+    WINDOW * mainWin = newwin(height, width, ypos, xpos);
+    box(mainWin, 0,0);
+    refresh();
+}
--- video_poker/poker.h
+++ video_poker/poker.h
@@ -6,8 +6,25 @@
 #define CS045_POKER_H
 
 #include <iostream>
+#include <ncurses.h>
 
 namespace poker{
+
+    struct playerHand {
+        unsigned int faceValue[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0},
+                suitValue[4] = {0,0,0,0},
+                pairs = 0;
+
+        bool royalStraightFlush = false;
+        bool hasAceKing = false;
+        bool fourOfAKind = false;
+        bool threeOfAKind = false;
+        bool straight = false;
+        bool flush = false;
+        bool fullHouse = false;
+
+    };
+
     void main();
     void loadCardValueAndSuits(std::string faceValue[], std::string suitValue[]);
     void shuffleDeck(bool deck[]);
@@ -15,15 +32,13 @@ namespace poker{
     void dealAHand(bool deck[], unsigned int hand[5]);
     void displayHand(unsigned int hand[5], std::string suitValue[], std::string faceValue[]);
     void scoreHand(unsigned int hand[5]);
-    void checkForHand(  unsigned int faceValue[],
-                        unsigned int suitValue[],
-                        bool &fourOfAKind,
-                        bool &threeOfAKind,
-                        unsigned int &pairs,
-                        bool &straight,
-                        bool &flush,
-                        bool &lastTwoAceKing);
+    void checkForHand(playerHand * my);
     void scoreHand(unsigned int hand[5]);
+    void initGame();
+    void exitGame();
+    void displayMainWindow();
+
+
 }
 
 #endif //CS045_POKER_H
